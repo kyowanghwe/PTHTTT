db7fd0a7ff082f102cd9c70ed067ac4c3dd54747
kyowanghwe <kieuquanghuy1999@gmail.com>
2021-05-04 17:16:15 +0700

    Update Donhang_Controller.java

--- PTHTTT/src/main/java/web/controller/Donhang_Controller.java
+++ PTHTTT/src/main/java/web/controller/Donhang_Controller.java
@@ -1,5 +1,20 @@
 package web.controller;
 
-public class Donhang_Controller {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import web.repo.DonhangRepository;
+
+@Controller
+@RequestMapping("/donhang")
+public class Donhang_Controller {
+	private final DonhangRepository donhangRepo;
+	
+	@Autowired
+	public Donhang_Controller(DonhangRepository donhangRepo) {
+		this.donhangRepo = donhangRepo;
+	}
+	
+	
 }
